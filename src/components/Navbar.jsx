14aa96652f2342832e5f32fef4532e71dbaec4be
Bongani <bbongs421@gmail.com>
2023-08-01 00:33:15 +0200

    rename website logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className='flex justify-between py-2'>
-        <h1 className='text-blue-600 font-logo font-bold text-3xl'><Link to='/'>B</Link></h1>
+        <h1 className='text-cyan-600 font-logo font-bold text-3xl'><Link to='/'>B <span className=' text-sm uppercase italic'>Portfolio</span></Link></h1>
         <ul className='flex items-center gap-5'>
             <li className='font-bold uppercase hover:text-cyan-500'><a href='#home'>Home</a></li>
             <li className='font-bold uppercase hover:text-cyan-500 cursor-pointer'><a href='#about'>About</a></li>
