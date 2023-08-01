7ad8110dadb4a9c9e63e4a35cb92e6c88360d3b4
Bongani <bbongs421@gmail.com>
2023-08-01 21:41:36 +0200

    change layout

--- src/components/Layout/homepage/Projects.jsx
+++ src/components/Layout/homepage/Projects.jsx
@@ -5,17 +5,17 @@ const Projects = () => {
   return (
     <section id='projects' className='py-10 mt-4'>
       <h2 className='section-heading text-center'>Projects</h2>
-        <div className='grid lg:grid-cols-2 gap-20 mt-10'>
+        <div className=' mt-10'>
             {projects.map(project => {
                 return (
-                    <div className='' key={project.id}>
-                        <div className=''>
-                            <img className='shadow-xl object-cover' src={project.img} alt={project.title} />
+                    <div className='lg:grid lg:grid-cols-3 gap-40 place-items-center mb-10' key={project.id}>
+                        <div className={` col-span-2 ${project.id % 2 != 0 ? "lg:order-1" : ""}`}>
+                            <img className='shadow-xl rounded mx-auto' src={project.img} alt={project.title} />
                         </div>
                         <div className=''>
                             <h3 className='text-center text-xl text-gray-700 font-bold font-logo mt-5'>{project.title}</h3>
                             <p className='text-center text-sm text-gray-500 italic'>{project.tools}</p>
-                            <p className='text-center text-gray-700 px-5'>{project.descrition}</p>
+                            <p className='text-center text-gray-700 px-5 sm:p-0'>{project.descrition}</p>
                             <div className='text-center mt-4'>
                                 <button className='btn border-none mr-10 bg-gradient-to-l from-cyan-200'>
                                     <a href={project.code} target='_blank'>Code</a>
