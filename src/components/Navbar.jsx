33f314dd08f9e94c10f78cf698b899d71c290f75
Bongani <bbongs421@gmail.com>
2023-08-05 12:47:41 +0200

    update the about section and make the navbar sticky

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between py-2'>
+    <nav className='bg-white z-50 flex justify-between py-2 sticky top-0'>
         <h1 className='text-cyan-600 font-logo font-bold text-3xl'><Link to='/'>B</Link></h1>
         <ul className='flex items-center gap-5'>
-            <li className='font-bold uppercase hover:text-cyan-500'><a href='#home'>Home</a></li>
             <li className='font-bold uppercase hover:text-cyan-500 cursor-pointer'><a href='#about'>About</a></li>
             <li className='font-bold uppercase hover:text-cyan-500'><a href='#projects'>Projects</a></li>
-            <li className='font-bold uppercase hover:text-cyan-500 cursor-pointer'><a href='#contact'>Contact</a></li>
+            <button className='text-white border-2 bg-cyan-400 border-cyan-400 rounded font-bold hover:text-cyan-400 hover:bg-white px-3 py-1 transition ease-out hover:scale-110'><a href='#contact'>Contact</a></button>
         </ul>
     </nav>
   )
