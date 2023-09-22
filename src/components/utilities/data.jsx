212c2761cd13c83233bee16d8c23d7db9b1c7fc6
Bongani <bbongs421@gmail.com>
2023-09-22 13:43:33 +0200

    add more projects

--- src/components/utilities/data.jsx
+++ src/components/utilities/data.jsx
@@ -1,16 +1,19 @@
-import littleLemon from '../../assets/little-lemon-restaurant.png'
+import netflix from '../../assets/netflix.png'
 import chatUp from '../../assets/chatup.png';
 import weather from '../../assets/weather.png';
+import twitter from '../../assets/twitter.png';
+import tesla from '../../assets/tesla.png';
+import googleKeep from '../../assets/google-keep.png';
 
 export const projects = [
     {
         id: 1,
-        title: 'Little Lemon Restaurant',
-        descrition: 'A fictional restaurant for reserving a table to dine later in the restaurant',
-        code: 'https://github.com/Bongani001/little-lemon-web-app',
-        live: 'https://bongani001.github.io/little-lemon-web-app/',
-        img: littleLemon,
-        tools: 'HTML/CSS | JavaScript | React'
+        title: 'Netflix clone',
+        descrition: 'A website that looks like the official website of Neflix',
+        code: 'https://github.com/Bongani001/netflix',
+        live: 'https://gelo-netflix.netlify.app',
+        img: netflix,
+        tools: 'HTML | CSS'
     },
     {
         id: 2,
@@ -23,11 +26,38 @@ export const projects = [
     },
     {
         id: 3,
+        title: 'Twitter',
+        descrition: 'A website that looks like the official website of Twitter',
+        code: 'https://github.com/Bongani001/twitter-clone',
+        live: 'https://chatupweb.netlify.app',
+        img: twitter,
+        tools: 'HTML | CSS'
+    },
+    {
+        id: 4,
         title: 'Weather',
         descrition: 'A weather app showing the current weather conditions using the OpenWeatherMap API',
         code: 'https://github.com/Bongani001/weather-app',
         live: 'https://bongani001.github.io/weather-app/',
         img: weather,
         tools: 'HTML/CSS | JavaScript | API'
+    },
+    {
+        id: 5,
+        title: 'Tesla',
+        descrition: 'A website that looks like the official website of Tesla',
+        code: 'https://github.com/Bongani001/tesla-clone',
+        live: 'https://gelo-tesla.netlify.app',
+        img: tesla,
+        tools: 'HTML | CSS'
+    },
+    {
+        id: 6,
+        title: 'Google Keep',
+        descrition: 'A website that looks like the official website of Google Keep',
+        code: 'https://github.com/Bongani001/google-keep-clone',
+        live: 'https://gelo-google-keep.netlify.app',
+        img: googleKeep,
+        tools: 'HTML | CSS'
     }
 ]
