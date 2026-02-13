547d39bc29d8b8f31bae2bf4017630455cdaa031
Bongani Nyaku <bbongs421@gmail.com>
2026-02-13 11:32:13 +0200

    Update projects.jsx

--- src/components/utilities/data.jsx
+++ src/components/utilities/data.jsx
@@ -1,46 +1,46 @@
-import netflix from '../../assets/netflix.png'
+import featherblog from '../../assets/featherblog.png'
 import chatUp from '../../assets/chatup.png';
-import weather from '../../assets/weather.png';
-import twitter from '../../assets/twitter.png';
-import tesla from '../../assets/tesla.png';
-import googleKeep from '../../assets/google-keep.png';
-import instagram from '../../assets/instagram.png';
+import anglelink from '../../assets/anglelink.png';
+import ctnprojects from '../../assets/ctnprojects.png';
+// import tesla from '../../assets/tesla.png';
+// import googleKeep from '../../assets/google-keep.png';
+// import instagram from '../../assets/instagram.png';
 
 export const projects = [
     {
         id: 1,
-        title: 'Netflix clone',
-        descrition: 'A website that looks like the official website of Neflix',
-        code: 'https://github.com/Bongani001/netflix',
-        live: 'https://gelo-netflix.netlify.app',
-        img: netflix,
-        tools: 'HTML | CSS'
+        title: 'Feather Blog',
+        descrition: 'A full-stack blogging platform built with the MERN stack',
+        code: 'https://github.com/Bongani001/mern-blog',
+        live: 'https://featherblog.netlify.app',
+        img: featherblog,
+        tools: 'React | TailwindCSS | NodeJS (Express) | MongoDB | Passport.js | JWT | Zustand (State Management)'
     },
     {
         id: 2,
+        title: 'CTN Projects (Client website)',
+        descrition: 'Air filtration solutions and laboratory equipment serving commercial, industrial, and scientific sectors across South Africa and beyond',
+        code: 'https://www.ctnprojects.co.za',
+        live: 'https://www.ctnprojects.co.za',
+        img: ctnprojects,
+        tools: 'React | TailwindCSS | SMTP (Email service)'
+    },
+    {
+        id: 3,
+        title: 'Angle Link',
+        descrition: 'Provide comprehensive IQ Retail Systems that streamline your business operations, enhance efficiency, and boost profitability',
+        code: 'https://angle-link.netlify.app',
+        live: 'https://angle-link.netlify.app',
+        img: anglelink,
+        tools: 'React | TailwindCSS | SMTP (Email service)'
+    },
+    {
+        id: 4,
         title: 'ChatUp',
         descrition: 'A full-stack mobile friendly chatting app similar to desktop WhatsApp',
         code: 'https://github.com/Bongani001/chatup',
         live: 'https://chatupweb.netlify.app',
         img: chatUp,
         tools: 'HTML/CSS | Bootstrap | JavaScript | React | Firebase'
-    },
-    {
-        id: 3,
-        title: 'Twitter Clone',
-        descrition: 'A website that looks like the official website of Twitter',
-        code: 'https://github.com/Bongani001/twitter-clone',
-        live: 'https://gelo-twitter.netlify.app',
-        img: twitter,
-        tools: 'HTML | CSS'
-    },
-    {
-        id: 5,
-        title: 'Tesla Website Clone',
-        descrition: 'A website that looks like the official website of Tesla',
-        code: 'https://github.com/Bongani001/tesla-clone',
-        live: 'https://gelo-tesla.netlify.app',
-        img: tesla,
-        tools: 'HTML | CSS'
-    },
+    }
 ]
